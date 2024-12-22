52b5d42a2045cc66b6b6cf0ca0f572004e8e309b
Luca Eliseev <84640571+PrimeOleg@users.noreply.github.com>
2024-12-22 03:28:26 +0000

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,43 @@
+from art import logo
+
+def add(n1, n2):
+    return n1 + n2
+# TODO: Write out the other 3 function - subtract, multiply, divide
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+  
+operations = {"+": add,
+              "-": subtract,
+              "*": multiply,
+              "/": divide
+}
+
+def calculator():
+    print(logo)
+
+    replay = True
+    number1 = float(input("What's the first number: "))
+
+    while True:
+        for operation in operations:
+            print(operation)
+        operation = input(f"Enter operation: ")
+        number2 = float(input("What's the next number: "))
+        result = operations[operation](number1, number2)
+        print(f"{number1} + {number2} = {result}")
+        continue_with_previous = input(f"Type 'y' to continue with {result}, or type 'n' to start a new calculation: ")
+
+        if continue_with_previous == "y":
+            number1 = result
+        else:
+            replay = False
+            print("\n" * 20)
+            calculator()
+
+calculator()
